214638a47b85e25b6a277028c318f6ea52f4c500
votemha <votemha.artem@gmail.com>
2024-01-29 19:50:00 +0300

    First commit in profile

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
         include 'general/mainPanel1.php'
     ?>
     <!-- основной контент страницы -->
-    <div class="mainContent">что-то здесь</div>
+    <div class="mainContent">что-то поменялось</div>
     <!-- Продолжение основы -->
     <?php
         include 'general/mainPanel2.php'
